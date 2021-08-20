d16c9a05dc9ba6034a1605e5831bd44a154ca2ad
Nan <nan@pop-os.localdomain>
2021-08-20 13:28:42 +0300

    complete

--- thirdtry.py
+++ thirdtry.py
@@ -1,19 +1,20 @@
-dataset ={
-        "foo": ["bar", "baz"],
-        "orange": ["bnanana", "mango"],
-        "bar": ["qux", "quux"],
-        "monkey": ["cow", "parrot"],
-        "banana": ["mango"],
-        "mango": ["banana", "foo"],
-        "baz": ["mango"],
-        "quux": ["bar", "banana"]
+
+dataset = {
+    "foo": ["bar", "baz"],
+    "orange": ["bnanana", "mango"],
+    "bar": ["qux", "quux"],
+    "monkey": ["cow", "parrot"],
+    "mango": ["banana"],
+    "baz": ["spam"],
+    "spam": ["mango"],
+    "banana": ["baz"],
+    "quux": ["bar", "banana"]
 }
+
 almost_solved = []
-list_of_lists = []
 
 def recursive_find(key, so_far):
     global almost_solved
-    print(so_far)
     for sub_key in dataset[key]:
         try:
             if sub_key in so_far:
@@ -25,8 +26,10 @@ def recursive_find(key, so_far):
         except KeyError as e:
             so_far.pop(-1)
             continue
+
+
 for key in dataset.keys():
-    recursive_find(key, [key])
+    recursive_find(key, [])
 
 intermediate_solution = []
 for solution in almost_solved:
@@ -34,7 +37,7 @@ for solution in almost_solved:
 
 solutions = []
 
-while len(intermediate_solution)> 0:
+while len(intermediate_solution) > 0:
     st = intermediate_solution.pop()
     if sorted(list(st)) not in [sorted(solution) for solution in solutions]:
         solutions.append(list(st))
