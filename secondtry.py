a03fefcb8064f7d20e1b7ebc0308df67e2c751a9
Nan <nan@pop-os.localdomain>
2021-08-20 10:32:51 +0300

    getting there

--- secondtry.py
+++ secondtry.py
@@ -13,18 +13,29 @@ almost_solved = []
 
 def recursive_find(key):
     global almost_solved
-    #print(ntry)
-    #total_tries.append([key])
     for sub_key in dataset[key]:
         try:
-            #total_tries[index].append(sub_key)
             if (key == sub_key) or (key in dataset[sub_key]) or (sub_key in dataset[sub_key]):
-                almost_solved.append([key, sub_key, dataset[sub_key][0]])
-                #print(key, sub_key, dataset[sub_key])
+                for index, name in enumerate(dataset[sub_key]):
+                    if name == key or name == sub_key:
+                        cycle_index = index
+                almost_solved.append([key, sub_key, dataset[sub_key][cycle_index]])
             return recursive_find(dataset[sub_key])
         except:
             continue
 for key in dataset.keys():
     recursive_find(key)
 
+the_solutions = []
+'''
+for solution in almost_solved:
+    to_compare = solution
+    for solution in almost_solved:
+        if to_compare == solution:
+            continue
+        else:
+            for i in range(len(solution)):
+                rotated = roll(solution, 1)
+                print(rotated)
+'''
 print(almost_solved)
